bad21b68446423ad36a35658fe7f0272281afb34
zlatakjshkarova <147971125+zlatakjshkarova@users.noreply.github.com>
2024-10-05 14:53:01 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+let a = '10';
+let b = '20';
+ab = a + b;
+console.log(ab);
+ab = +a + +b;
+console.log(ab);
+let d = '5px';
+let s = '6px';
+d = parseInt(d);
+s = parseInt(s);
+console.log(`${d + s}px`);
+
+
+let f = '5.5px';
+f = parseInt(f);
+let g = '6.25px';
+g = parseInt(g);
+
+console.log(`${f + g}px`);
+
+let vb = '5.5px';
+let vc = '6.25px';
+vb = parseInt(vb);
+vc = parseInt(vc);
+console.log(`${vb + vc}px`);
+
+let mn = 11.75;
+mn = String(mn);
+console.log(`'${mn}px'`);
+
+let bn = 4;
+let bg = 5;
+
+bn = String(bn);
+bg = String(bg);
+vbn = (bn + bg);
+console.log(vbn);
+vbn = +bn + +bg;
+console.log(vbn);
+
+console.log(`${(Number(pl))} * 2 * r`);
+
+let str = 'string';
+console.log(str[2]);
+str[2] = 'q'
+console.log(str);
+str1 = 'String';
+console.log(st.toLowerCase() == str1.toLowerCase());
+console.log(str.toLocaleLowerCase());
+console.log(str.toLocaleUpperCase());
+//
+console.log(str.length);
